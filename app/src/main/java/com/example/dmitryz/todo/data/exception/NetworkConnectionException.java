4381c0265e3f0b07a27d6507d6aea28eb85befc2
q <q>
2017-06-11 03:14:52 +0700

    add data/exceptions

---
+++ app/src/main/java/com/example/dmitryz/todo/data/exception/NetworkConnectionException.java
@@ -0,0 +1,15 @@
+package com.example.dmitryz.todo.data.exception;
+
+/**
+ * Created by dmitryz on 6/2/17.
+ */
+
+public class NetworkConnectionException extends Exception {
+    public NetworkConnectionException() {
+        super();
+    }
+
+    public NetworkConnectionException(final Throwable cause) {
+        super(cause);
+    }
+}
---
+++ app/src/main/java/com/example/dmitryz/todo/data/exception/ToDoItemNotFound.java
@@ -0,0 +1,11 @@
+package com.example.dmitryz.todo.data.exception;
+
+/**
+ * Created by dmitryz on 6/2/17.
+ */
+
+public class ToDoItemNotFound extends Exception {
+    public ToDoItemNotFound() {
+        super();
+    }
+}
